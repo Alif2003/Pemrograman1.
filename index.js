19bdc90af1d9b076b6abb2d1b9b0b5879fbe4e29
muhammadalif17 <114787254+Alif2003@users.noreply.github.com>
2023-09-27 12:00:44 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//Node.js 10.14.0
+//Plain Javascript and Node.js is supported
+// html/css is not supported here 
+// Deklarasi variabel dengan nama dan NIM Anda
+const nama = "Muhammad Alif Apriza";
+const nim = "20220040074";
+
+// Deklarasi variabel dengan tipe data number
+const angka1 = 167;
+const angka2 = 21;
+
+// Melakukan operasi aritmatika sederhana
+const tambah = angka1 + angka2;
+const kurang = angka1 - angka2;
+const kali = angka1 * angka2;
+const bagi = angka1 / angka2;
+
+// Menampilkan identitas dan hasil operasi aritmatika
+console.log(`Nama: ${nama}`);
+console.log(`NIM: ${nim}`);
+console.log(`Hasil Penjumlahan: ${tambah}`);
+console.log(`Hasil Pengurangan: ${kurang}`);
+console.log(`Hasil Perkalian: ${kali}`);
+console.log(`Hasil Pembagian: ${bagi}`);
+console.log("index!")
